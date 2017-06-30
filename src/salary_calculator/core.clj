51a80f416e9cccb5bbb279dfdaa76605e1471e82
Grace <gracem@thoughtworks.com>
2017-06-30 11:11:15 +0200

    Hello World by runnable program

--- src/salary_calculator/core.clj
+++ src/salary_calculator/core.clj
@@ -1,6 +1,6 @@
 (ns salary-calculator.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "I can say 'Hello World'."
+  []
+  (println "Hello, World!"))
